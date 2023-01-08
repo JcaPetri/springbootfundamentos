54fbc1a750f28e7a82b1041399e33f3656f69a80
Juan Petri <juancpetri@hotmail.com>
2023-01-08 16:39:49 -0300

    First Commit

---
+++ src/main/java/com/springbootfundamentos/SpringbootfundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.springbootfundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfundamentosApplication.class, args);
+	}
+
+}
